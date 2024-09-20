48dfca597e5715dea36156cc0e377c415e68ea20
lemengbin <lemengbin@163.com>
2024-09-20 10:43:52 +0800

    fix index error

--- Sources/web3swift/EthereumAPICalls/Safe4/types/safe3/LockedSafe3Info.swift
+++ Sources/web3swift/EthereumAPICalls/Safe4/types/safe3/LockedSafe3Info.swift
@@ -25,11 +25,11 @@ public struct LockedSafe3Info: Safe4T {
         guard values.count == 7,
               let safe3Addr = values[0] as? String,
               let amount = values[1] as? BigUInt,
-              let remainLockHeight = values[5] as? BigUInt,
-              let lockDay = values[6] as? BigUInt,
-              let isMN = values[7] as? Bool,
-              let safe4Addr = values[8] as? EthereumAddress,
-              let redeemHeight = values[9] as? BigUInt
+              let remainLockHeight = values[2] as? BigUInt,
+              let lockDay = values[3] as? BigUInt,
+              let isMN = values[4] as? Bool,
+              let safe4Addr = values[5] as? EthereumAddress,
+              let redeemHeight = values[6] as? BigUInt
          else { return nil }
          self.init(safe3Addr: safe3Addr, amount: amount, remainLockHeight: remainLockHeight, lockDay: lockDay, isMN: isMN, safe4Addr: safe4Addr, redeemHeight: redeemHeight)
     }
