2837416565c2e4ce0c86ac0ef9fac777c89640be
lemengbin <lemengbin@163.com>
2025-04-23 10:58:21 +0800

    add getImmatureBalance for Proposal

--- Tests/web3swiftTests/localTests/ProposalTest.swift
+++ Tests/web3swiftTests/localTests/ProposalTest.swift
@@ -12,6 +12,13 @@ class ProposalTest: LocalTestCase {
         XCTAssertTrue(ret > 0)
     }
 
+    func testGetImmatureBalance() async throws {
+        let web3 = try await Web3.new(LocalTestCase.url, network: Networks.fromInt(6666666))
+        let ret = try await web3.safe4.proposal.getImmatureBalance()
+        print(ret)
+        XCTAssertTrue(ret > 0)
+    }
+
     func testCreate() async throws {
         let web3 = try await Web3.new(LocalTestCase.url, network: Networks.fromInt(6666666))
         // addr: 0xd52114c4071b5bfbd06a657a3db538bfd559a481, privateKey: 0xe171bcc091332eaa8fd76f529f880bd210b3187354706599b033df8155a94d84
