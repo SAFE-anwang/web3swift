5ae1d2d8652ce9ef1cc7769fdeae5b84082aad90
lemengbin <lemengbin@163.com>
2025-06-25 18:29:50 +0800

    height => unlockHeight in MemberInfo

--- Sources/web3swift/EthereumAPICalls/Safe4/types/supernode/SuperNodeMemberInfo.swift
+++ Sources/web3swift/EthereumAPICalls/Safe4/types/supernode/SuperNodeMemberInfo.swift
@@ -5,13 +5,13 @@ public struct SuperNodeMemberInfo {
     public var lockID: BigUInt
     public var addr: EthereumAddress
     public var amount: BigUInt
-    public var height: BigUInt
+    public var unlockHeight: BigUInt
 
-    public init(lockID: BigUInt, addr: EthereumAddress, amount: BigUInt, height: BigUInt) {
+    public init(lockID: BigUInt, addr: EthereumAddress, amount: BigUInt, unlockHeight: BigUInt) {
         self.lockID = lockID
         self.addr = addr
         self.amount = amount
-        self.height = height
+        self.unlockHeight = unlockHeight
     }
 
     public init?(values: [Any]) {
@@ -19,8 +19,8 @@ public struct SuperNodeMemberInfo {
               let lockID = values[0] as? BigUInt,
               let addr = values[1] as? EthereumAddress,
               let amount = values[2] as? BigUInt,
-              let height = values[3] as? BigUInt
+              let unlockHeight = values[3] as? BigUInt
          else { return nil }
-         self.init(lockID: lockID, addr: addr, amount: amount, height: height)
+         self.init(lockID: lockID, addr: addr, amount: amount, unlockHeight: unlockHeight)
     }
 }
